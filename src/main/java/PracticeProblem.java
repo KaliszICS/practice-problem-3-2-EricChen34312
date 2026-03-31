12dd4848b16b159f2a9335414cc583d9d208ab38
EricChen34312 <85235002+EricChen34312@users.noreply.github.com>
2026-03-31 13:30:30 +0000

    aaaaaaaaaaaaaaaasolve

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,18 @@ public class PracticeProblem {
 
 	}
 
+	public static int towerOfHanoi(int num){
+
+		//if num < 3 return -1
+		if(num < 3){
+			return -1; 
+		} else if (num == 3){
+			//base case of num == 3, as the min number of moves goes (1,3,7)
+			return 7;
+		}
+
+		//since the min number of moves is 2x + 1 from the last, recursively call the function with num-1
+		return towerOfHanoi(num-1)*2+1;
+	}
+
 }
